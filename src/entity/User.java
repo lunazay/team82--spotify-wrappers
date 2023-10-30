14ea355e9809cf20ece53616d74b63feac0c91de
yoonie-jang <yoonie.jang@mail.utoronto.ca>
2023-10-30 12:45:43 -0400

    renamed some stuff

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,7 +1,7 @@
 package entity;
 
 public class User extends DataObject {
-    // Parameters id and name from DataObject.
+    // Parameters id and name from DataObject. ok
 
     public User(String id) {
         super(id);
