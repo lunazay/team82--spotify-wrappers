a84c7527aa5eb6b15ead6fef800ffbf12b61fe9a
yoonie-jang <yoonie.jang@mail.utoronto.ca>
2023-10-30 12:09:21 -0400

    added User.java class

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,4 +1,23 @@
 package entity;
 
-public interface User {
+public class User {
+
+    private final String userID;
+
+    private final String displayName;
+
+
+    public User(String userID, String displayName) {
+
+        this.userID = userID;
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName(){
+        return this.displayName;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
 }
