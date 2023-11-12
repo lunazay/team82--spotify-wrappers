f3551e58ae90db5b9ecebb846a5ff14bbb327a87
lunazayed <luna.zayed@mail.utoronto.ca>
2023-11-11 21:47:06 -0500

    changes in imput boundary and input data

--- src/use_case/TopSongs/TopSongsInputData.java
+++ src/use_case/TopSongs/TopSongsInputData.java
@@ -1,4 +1,18 @@
 package use_case.TopSongs;
+import entity.User;
 
 public class TopSongsInputData {
+    private String timeframe;
+
+    public TopSongsInputData(String timeframe){
+        this.timeframe = timeframe;
+    }
+
+    public void setTimeframe(String timeframe) {
+        this.timeframe = timeframe;
+    }
+
+    public String getTimeframe() {
+        return timeframe;
+    }
 }
