bf689d2f7a8e9221c4bbfae051fbfc976b130cf4
ashleychristen <ashley.christendat@mail.utoronto.ca>
2023-11-13 12:41:59 -0500

    edited api calls

--- src/api/DevelopDB.java
+++ src/api/DevelopDB.java
@@ -1,4 +1,11 @@
 package api;
 
+import entity.User;
+import entity.Artist;
+import org.json.JSONException;
+
 public interface DevelopDB {
+    User getTopSongs();
+
+    User getTopArtist();
 }
--- src/api/SpotDevelopDB.java
+++ src/api/SpotDevelopDB.java
@@ -1,4 +1,15 @@
 package api;
 
+import entity.User;
+
 public class SpotDevelopDB implements DevelopDB{
+    @Override
+    public User getTopArtist() {
+        return null;
+    }
+
+    @Override
+    public User getTopSongs() {
+        return null;
+    }
 }
