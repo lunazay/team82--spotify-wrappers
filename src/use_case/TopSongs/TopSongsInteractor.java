5314b283416ae5b72030df09cb9c4d5a824e50a8
jackwiebe0 <jackwiebe@me.com>
2023-11-13 12:22:54 -0500

    implemented interactor

--- src/use_case/TopSongs/TopSongsInteractor.java
+++ src/use_case/TopSongs/TopSongsInteractor.java
@@ -2,6 +2,9 @@ package use_case.TopSongs;
 
 import entity.Song;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TopSongsInteractor implements TopSongsInputBoundary {
 
     final TopSongsDataAccessInterface userDataAccessObject;
@@ -20,9 +23,15 @@ public class TopSongsInteractor implements TopSongsInputBoundary {
 
         try {
             Song[] topSongs = userDataAccessObject.getTopSongs(id, timeframe);
+            List<String> topSongTitles = new ArrayList<String>(50);
 
+            int i = 0;
+            for (Song song : topSongs) {
+                topSongTitles.set(i, song.getName());
+                i = i + 1;
+            }
 
-            TopSongsOutputData topSongsOutputData = new TopSongsOutputData();
+            TopSongsOutputData topSongsOutputData = new TopSongsOutputData(id, topSongTitles, false);
             topSongsPresenter.prepareSuccessView(topSongsOutputData);
         } catch (Exception e) {
             topSongsPresenter.prepareFailView("Listen to some music bro!");
