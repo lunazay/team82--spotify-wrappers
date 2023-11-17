41b349ef02e9387b2b40411ee459e83679359c6c
jackwiebe0 <jackwiebe@me.com>
2023-11-17 17:02:23 -0500

    implemented TopArtistOutputData

--- src/use_case/TopArtist/TopArtistOutputData.java
+++ src/use_case/TopArtist/TopArtistOutputData.java
@@ -1,4 +1,19 @@
 package use_case.TopArtist;
 
+import java.util.List;
+
 public class TopArtistOutputData {
+    private List<String> artistNames;
+
+    private boolean useCaseFailed;
+
+    public TopArtistOutputData(List<String> artistNames, boolean useCaseFailed) {
+        this.artistNames = artistNames;
+        this.useCaseFailed = useCaseFailed;
+
+    }
+
+    public List<String> getArtistNames() {
+        return artistNames;
+    }
 }
