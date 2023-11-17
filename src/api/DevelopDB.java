4d5c686151458a8a23cfad57eb7a285c06a459d0
yoonie-jang <yoonie.jang@mail.utoronto.ca>
2023-11-17 16:03:53 -0500

    implemented getAuthorizationToken() and getAuthorizationLink()

--- src/api/DevelopDB.java
+++ src/api/DevelopDB.java
@@ -8,9 +8,8 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 
 public interface DevelopDB {
-    String client_id = "bad90b33466e4f208c7655eede3ac628";
-    String client_secret = "15abfd5161e84bfe893606e4eb74f5f6";
-    String redirect_uri = "https://oauth.pstmn.io/v1/browser-callback";
+
+    void setAuthToken(String authToken);
     User getTopSongs();
 
     User getTopArtist();
