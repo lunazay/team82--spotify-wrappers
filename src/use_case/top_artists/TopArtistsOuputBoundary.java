a941aea4b11f2537b6171379cdadf2c83f6b5987
jackwiebe0 <jackwiebe@me.com>
2023-11-17 17:17:42 -0500

    added JavaDoc

--- src/use_case/top_artists/TopArtistsOuputBoundary.java
+++ src/use_case/top_artists/TopArtistsOuputBoundary.java
@@ -1,7 +1,16 @@
 package use_case.top_artists;
 
 public interface TopArtistsOuputBoundary {
-    void prepareSuccessView(TopArtistsOutputData user);
 
+    /**
+     * Updates the view with the list of TopArtists received.
+     * @param artists Stores the list of the names of the user's top artists.
+     */
+    void prepareSuccessView(TopArtistsOutputData artists);
+
+    /**
+     * Updates the view with an error to be displayed.
+     * @param error The error to show to the user
+     */
     void prepareFailView(String error);
 }
