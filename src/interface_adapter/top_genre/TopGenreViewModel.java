4006f92128b4f2549a4b5e01fd893f24cdfbab5e
lunazayed <luna.zayed@mail.utoronto.ca>
2023-11-19 14:41:32 -0500

    small change in the top genre view model

--- src/interface_adapter/top_genre/TopGenreViewModel.java
+++ src/interface_adapter/top_genre/TopGenreViewModel.java
@@ -6,18 +6,15 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class TopGenreViewModel extends ViewModel {
-
-    private static final String TITLE_LABLE = "Top Genres View";
     private TopGenreState state = new TopGenreState();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
     public TopGenreViewModel(){
         super("Top Genres");
     }
-
     public void setState(TopGenreState state){
         this.state = state;
     }
-    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
     // the Top genre presenter will call this to let the viewmodel to alert the view
     public void firePropertyChanged(){
         support.firePropertyChange("state", null, this.state);
