d3477362d5b37235066668601ee86717b3113537
Veda Kesarwani <vedakesarwani@gmail.com>
2023-11-19 23:07:09 -0500

    Implemented TopAlbumController

--- src/interface_adapter/top_album/TopAlbumController.java
+++ src/interface_adapter/top_album/TopAlbumController.java
@@ -1,4 +1,18 @@
 package interface_adapter.top_album;
 
+import use_case.TopAlbum.TopAlbumInputBoundary;
+import use_case.TopAlbum.TopAlbumInputData;
+
 public class TopAlbumController {
+    final TopAlbumInputBoundary topAlbumUseCaseInteractor;
+
+    public TopAlbumController(TopAlbumInputBoundary topAlbumInputBoundary) {
+        this.topAlbumUseCaseInteractor = topAlbumInputBoundary;
+    }
+
+    public void execute(String timeframe, String id) {
+        TopAlbumInputData topAlbumInputData = new TopAlbumInputData(timeframe, id);
+
+        topAlbumUseCaseInteractor.execute(topAlbumInputData);
+    }
 }
