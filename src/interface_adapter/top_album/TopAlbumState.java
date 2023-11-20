ae2d8931049fbc35eed95c7456d514595d6d48e7
Veda Kesarwani <vedakesarwani@gmail.com>
2023-11-19 23:17:48 -0500

    Made some progress working on the state; adding few more methods

--- src/interface_adapter/top_album/TopAlbumState.java
+++ src/interface_adapter/top_album/TopAlbumState.java
@@ -1,8 +1,30 @@
 package interface_adapter.top_album;
 
+import interface_adapter.related_artists.RelatedArtistsState;
+
+import java.util.Collections;
+import java.util.List;
+
 public class TopAlbumState {
+
+    private List<String> topAlbumNames = Collections.emptyList();
+
+    private String error = null;
+    public TopAlbumState(TopAlbumState copy){
+        topAlbumNames = copy.topAlbumNames;
+        error = copy.error;
+    }
 
     public TopAlbumState() {}
 
     // TODO: complete the rest of the state
+
+    public void setError(String error){
+        this.error = error;
+    }
+
+
+
+
+
 }
