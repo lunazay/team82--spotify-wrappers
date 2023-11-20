390c2fbe3eee9ab10c0b6306cc4f996b3c49e2e6
lunazayed <luna.zayed@mail.utoronto.ca>
2023-11-20 18:12:46 -0500

    created some mroe files for testing the top genre use case

---
+++ test/use_case/TopGenre/TopGenreInputDataTest.java
@@ -0,0 +1,16 @@
+package use_case.TopGenre;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopGenreInputDataTest {
+
+    @Test
+    void getTimeframe() {
+    }
+
+    @Test
+    void getId() {
+    }
+}
