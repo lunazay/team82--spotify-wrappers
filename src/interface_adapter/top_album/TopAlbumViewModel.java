5d89350eaf15c338c5a80fbbffec0dc728d3e237
Veda Kesarwani <vedakesarwani@gmail.com>
2023-11-19 23:08:37 -0500

    Implemented the album view model

--- src/interface_adapter/top_album/TopAlbumViewModel.java
+++ src/interface_adapter/top_album/TopAlbumViewModel.java
@@ -1,4 +1,34 @@
 package interface_adapter.top_album;
 
-public class TopAlbumViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class TopAlbumViewModel extends ViewModel {
+    private static final String TITLE_LABEL = "Top Album View";
+
+    private TopAlbumState state = new TopAlbumState();
+
+    public TopAlbumViewModel() {
+        super("Top Album");
+    }
+
+    public void setState(TopAlbumState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener){
+        support.addPropertyChangeListener(listener);
+    }
+
+    public TopAlbumState getState() {
+        return state;
+    }
 }
