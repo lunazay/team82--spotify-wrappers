cf3ace1708ae2487dce9c4b131febe74bc570f2b
jackwiebe0 <jackwiebe@me.com>
2023-11-19 19:26:33 -0500

    implemented the TopArtistsState

--- src/interface_adapter/top_artists/TopArtistsState.java
+++ src/interface_adapter/top_artists/TopArtistsState.java
@@ -1,4 +1,33 @@
 package interface_adapter.top_artists;
 
+import java.util.Collections;
+import java.util.List;
+
 public class TopArtistsState {
+    private List<String> artistNames = Collections.emptyList();     // Sets default value as empty list.
+    private String artistNamesError = null;
+
+    public TopArtistsState(TopArtistsState copy) {
+        this.artistNames = copy.artistNames;
+        this.artistNamesError = copy.artistNamesError;
+    }
+
+    public TopArtistsState() {}
+
+    public void setArtistNames(List<String> artistNames) {
+        this.artistNames = artistNames;
+    }
+
+    public void setArtistNamesError(String artistNamesError) {
+        this.artistNamesError = artistNamesError;
+    }
+
+    public String getArtistNamesError() {
+        return this.artistNamesError;
+    }
+
+    @Override
+    public String toString(){
+        return "TopArtistsState{" + "artistNames='" + artistNames + '}';
+    }
 }
