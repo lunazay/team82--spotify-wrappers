81a9c96170f515824fb0314432ce97b95d24f5c0
Veda Kesarwani <vedakesarwani@gmail.com>
2023-11-19 23:08:18 -0500

    Implemented the album presenter

--- src/interface_adapter/top_album/TopAlbumPresenter.java
+++ src/interface_adapter/top_album/TopAlbumPresenter.java
@@ -1,4 +1,27 @@
 package interface_adapter.top_album;
 
-public class TopAlbumPresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.TopAlbum.TopAlbumOutputBoundary;
+import use_case.TopAlbum.TopAlbumOutputData;
+
+public class TopAlbumPresenter implements TopAlbumOutputBoundary {
+    private final TopAlbumViewModel topAlbumViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public TopAlbumPresenter(ViewManagerModel viewManagerModel,
+                             TopAlbumViewModel topAlbumViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.topAlbumViewModel = topAlbumViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(TopAlbumOutputData user) {
+        // TODO: need to finish this
+    }
+
+    @Override
+    public void prepareFailView(String error){
+        // TODO: need to finish this
+
+    }
 }
