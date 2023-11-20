077b646a8f779d7ad32abf9343ab5c2a90750831
jackwiebe0 <jackwiebe@me.com>
2023-11-20 12:07:21 -0500

    create ArtistFactory

---
+++ src/entity/ArtistFactory.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class ArtistFactory {
+    /**
+     * Creates a list of Artist objects from an API response
+     * @param response API call for a user's top artists
+     */
+    Artist[] create(String response) {
+
+        return null;
+    }
+}
