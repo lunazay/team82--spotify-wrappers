d8e17c8f14e6e3cc1d3bdbfde5f6303bc8bdefa0
jackwiebe0 <jackwiebe@me.com>
2023-11-20 12:22:06 -0500

    change super constructor to accept (id, name)

--- src/entity/DataObject.java
+++ src/entity/DataObject.java
@@ -4,12 +4,9 @@ public abstract class DataObject {
     private final String id;
     private final String name;
 
-    public DataObject(String id) {
+    public DataObject(String id, String name) {
         this.id = id;
-        this.name = "";      // TODO: API Call to set the name (song/artist have "name"
-                             // TODO: and user has "display_name")
-
-                             // Why not initialize these with a name parameter as well?
+        this.name = name;
     }
 
     public String getId() {
