bef3b64065b4805887cdbbfb4b6827c5dbe6bdfa
lunazayed <luna.zayed@mail.utoronto.ca>
2023-11-20 20:16:41 -0500

    attempted to start on the main file.

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,14 +3,25 @@ package app;
 import api.SpotDevelopDB;
 import api.DevelopDB;
 
+import data_access.UserDataAccessObject;
 import entity.User;
 import entity.Artist;
 import entity.Song;
 import entity.Genre;
 
+import interface_adapter.ViewManagerModel;
 import interface_adapter.ViewModel;
+import interface_adapter.get_valence.GetValenceViewModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.related_artists.RelatedArtistsViewModel;
+import interface_adapter.top_album.TopAlbumViewModel;
+import interface_adapter.top_artists.TopArtistsViewModel;
+import interface_adapter.top_genre.TopGenreViewModel;
+import interface_adapter.top_songs.TopSongsViewModel;
 import view.LoggedInView;
 import view.LoginView;
+import view.ViewManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,4 +30,43 @@ import java.io.IOException;
 
 
 public class Main {
+    public static void main(String[] args){
+        JFrame application = new JFrame("login Example");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        LoginViewModel loginViewModel = new LoginViewModel();
+        LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
+        GetValenceViewModel getValenceViewModel = new GetValenceViewModel();
+        RelatedArtistsViewModel relatedArtistsViewModel = new RelatedArtistsViewModel();
+        TopSongsViewModel topSongsViewModel = new TopSongsViewModel();
+        TopAlbumViewModel topAlbumViewModel = new TopAlbumViewModel();
+        TopArtistsViewModel topArtistsViewModel = new TopArtistsViewModel();
+        TopGenreViewModel topGenreViewModel = new TopGenreViewModel();
+
+        UserDataAccessObject userDataAccessObject;
+        try{
+            userDataAccessObject = new // TODO: input the factory
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        // i think we need to do this for every view? LoggedInView loggedInView =
+
+        LoggedInView loggedInView = new LoggedInView(loggedInViewModel);
+        views.add(loggedInView, loggedInView.viewName());
+
+        viewManagerModel.setActiveViewName(loggedInViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+
+        application.pack();;
+        application.setVisible(true);
+    }
 }
