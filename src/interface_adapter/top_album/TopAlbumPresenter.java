daeb1fc5f21a7a89f685a686b3ceaca1cde6dd40
Veda Kesarwani <vedakesarwani@gmail.com>
2023-11-20 21:10:08 -0500

    Removing unnecessary code and comments

--- src/interface_adapter/top_album/TopAlbumPresenter.java
+++ src/interface_adapter/top_album/TopAlbumPresenter.java
@@ -1,7 +1,6 @@
 package interface_adapter.top_album;
 
 import interface_adapter.ViewManagerModel;
-import interface_adapter.top_artists.TopArtistsState;
 import use_case.TopAlbum.TopAlbumOutputBoundary;
 import use_case.TopAlbum.TopAlbumOutputData;
 
@@ -17,7 +16,6 @@ public class TopAlbumPresenter implements TopAlbumOutputBoundary {
 
     @Override
     public void prepareSuccessView(TopAlbumOutputData user) {
-        // TODO: need to implement this
         TopAlbumState topAlbumState = topAlbumViewModel.getState();
         topAlbumState.setTopAlbumNames(user.getAlbumNames());
         this.topAlbumViewModel.setState(topAlbumState);
