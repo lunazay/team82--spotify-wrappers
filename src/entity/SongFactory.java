fea66c72f5250c123b744e8bc3ae7d34e37c0ca3
Veda Kesarwani <vedakesarwani@gmail.com>
2023-11-23 20:32:52 -0500

    delete unnecessary code

--- src/entity/SongFactory.java
+++ src/entity/SongFactory.java
@@ -14,8 +14,7 @@ public class SongFactory {
         for (int i = 0; i < response.length(); i++) {
             String id = response.getString("id");
             String name = response.getString("name");
-            List<String> songList = (List<String>) response.get("songs");
-            // TODO: I will completed this soon.
+            // TODO: I will complete this soon.
             //  I need to get the length, artist and album so the red on line 15 goes away
 
             songs[i] = new Song(id, name, length, artist, album);
