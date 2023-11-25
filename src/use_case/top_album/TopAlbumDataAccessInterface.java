718472bb9ffc4be9bd3a0f0e372a80dfbf02a8c1
jackwiebe0 <jackwiebe@me.com>
2023-11-24 19:32:24 -0500

    add exceptions to be thrown

--- src/use_case/top_album/TopAlbumDataAccessInterface.java
+++ src/use_case/top_album/TopAlbumDataAccessInterface.java
@@ -2,6 +2,6 @@ package use_case.top_album;
 import entity.Song;
 
 public interface TopAlbumDataAccessInterface {
-    Song[] getTopAlbums(String id, String timeframe);
+    Song[] getTopAlbums(String id, String timeframe) throws Exception;
 
 }
