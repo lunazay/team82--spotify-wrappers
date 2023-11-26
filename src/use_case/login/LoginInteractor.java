d28bc97a581ad65f1263646192783d86abf08b1f
yoonie-jang <yoonie.jang@mail.utoronto.ca>
2023-11-26 01:48:27 -0500

    fixed static issue

--- src/use_case/login/LoginInteractor.java
+++ src/use_case/login/LoginInteractor.java
@@ -17,9 +17,11 @@ public class LoginInteractor implements LoginInputBoundary{
         //TODO: fifure this out when input data is done
         String codeInput = loginInputData.getCodeInput();
 
-        // Sending the token to the DAO so it can be stored inside the program,
+        // Sending the token to the DAO so it can be stored inside the program:
+        userDataAccessInterface.setToken(codeInput);
+
         // and also getting the user id back:
-        User currentUser = LoginUserDataAccessInterface.getNewUser();
+        User currentUser = userDataAccessInterface.getNewUser();
         // Not sure why it's saying it needs to be static. - yj
 
         LoginOutputData loginOutputData = new LoginOutputData(currentUser);
