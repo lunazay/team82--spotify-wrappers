7c163fbec0fcd0b648c0517a379df91813d2f3c1
lunazayed <luna.zayed@mail.utoronto.ca>
2023-11-27 17:24:11 -0500

    finished the get Valence view model get view panel

--- src/interface_adapter/get_valence/GetValenceViewModel.java
+++ src/interface_adapter/get_valence/GetValenceViewModel.java
@@ -2,6 +2,10 @@ package interface_adapter.get_valence;
 
 import interface_adapter.ViewManagerModel;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 public class GetValenceViewModel extends ViewManagerModel {
 
     private GetValenceState state = new GetValenceState();
@@ -10,4 +14,23 @@ public class GetValenceViewModel extends ViewManagerModel {
 
     public GetValenceState getState() { return this.state; }
 
+    public JPanel getViewPanel() {
+        // Create and configure the JPanel for the top genre view
+        JPanel getValencePanel = new JPanel();
+        getValencePanel.setLayout(new BorderLayout());
+
+        // Add components representing the view
+        JLabel titleLabel = new JLabel("Valence"); // Update label accordingly
+        getValencePanel.add(titleLabel, BorderLayout.NORTH);
+
+        // Retrieve the valence value from the state
+        String valence = state.getValence();
+
+        // Display the valence value in the view
+        JLabel valenceLabel = new JLabel(valence != null ? valence : "No valence available");
+        valenceLabel.setHorizontalAlignment(SwingConstants.CENTER); // Center align the label
+        getValencePanel.add(valenceLabel, BorderLayout.CENTER);
+
+        return getValencePanel;
+    }
 }
