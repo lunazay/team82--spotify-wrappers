b20fcdd97de7b2b1b04538c7a1e7cda28f6c2b12
jackwiebe0 <jackwiebe@me.com>
2023-11-27 18:54:28 -0500

    merge main with rebase

--- test/use_case/TopArtistsTest.java
+++ test/use_case/TopArtistsTest.java
@@ -8,24 +8,21 @@ import java.util.Objects;
 import api.SpotDevelopDB;
 import data_access.UserDataAccessObject;
 import entity.Artist;
-import entity.Song;
 import org.junit.Assert;;
 
 public class TopArtistsTest {
 
     public static void main(String[] args) throws IOException {
         SpotDevelopDB api = new SpotDevelopDB();
-        System.out.println(api.getAuthorizationLink());
-        String authCode = "AQD8tCOuYth3H3LLWyfJOQb0ScNXj-bRBqvzNOFPt3_5gLQERNltntdoY8j2X5CrfUZSHaJVaz2UaU0WJtdR3nmwe986mIuRoPc2B5Mn1gfnqDE9GM8Om88XlJDvc83AwHIIK4yWNoLMWoWZO2Gxbxnk3Kmn2mmopYAUMh3YjQUg4I1ZhEYYLLJ6a4IcZNvNaWQ0TtarI3v5ZZhIIifq5L56G2fVDc1ZzbZxZs9HobbLSAyN4Id2kCNDe4mwkiB5C8IRG9GD2UsjTEoQslsLumTnP4vjIpTdvaHPHVrsXaNunhMxTF4pJJPRm231ZN5QeR8";
-        System.out.println(api.getAuthorizationToken(authCode));
-        System.out.println(api.getUserId());
+        System.out.println(api.getAuthorizationToken("AQChVsKdnjpC7T_Pkd5fIcsYnJXDXMENi0VZrC9FDKGyXTLy-VFtiJ3hcCY4Y2KP4XdKDU9b7YB_BqCs6vwmWfqPTP6smC4Z4zB1H9CrHOb5pZNx0g2i5EkQhj3-GMyQGLtMZT5PVUjKb7DAGSgH6FNOO4Tp0Gbvp1fMP6msJvJ7bzrsmtr69gnleMM-_e5OPDKPgLchFMiwC6Q9iB1iB8Ve7GXJkg6wuamy-hUCaQQ"));
     }
 
     @org.junit.Test
     public void testTopArtist() throws Exception {
         UserDataAccessObject userDataAccessObject = new UserDataAccessObject();
         SpotDevelopDB api = new SpotDevelopDB();
-        Artist[] topArtists = userDataAccessObject.getTopArtists("31kaxlo4ik6qs6vu3gtkb4i5iyye", "long_term");
+
+        Artist[] topArtists = userDataAccessObject.getTopArtists("0wn1qh223kptm533gbxyin4mh", "long_term");
         assert(Objects.equals(topArtists[0].getName(), "Paloma Faith"));
     }
 }
