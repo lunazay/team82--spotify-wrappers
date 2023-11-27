1d6514a725f51607925e3297502b5b0c2b61df6e
lunazayed <luna.zayed@mail.utoronto.ca>
2023-11-27 15:34:38 -0500

    here are my changes, please replicate them in your coresponding use case

--- src/interface_adapter/top_genre/TopGenreViewModel.java
+++ src/interface_adapter/top_genre/TopGenreViewModel.java
@@ -1,9 +1,13 @@
 package interface_adapter.top_genre;
 
+import entity.Genre;
 import interface_adapter.ViewModel;
 
+import javax.swing.*;
+import java.awt.*;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
 
 public class TopGenreViewModel extends ViewModel {
 
@@ -17,6 +21,10 @@ public class TopGenreViewModel extends ViewModel {
     public void setState(TopGenreState state){
         this.state = state;
     }
+
+    public ArrayList<Genre> getgenres(){
+        return state.getGenres();
+    }
     // the Top genre presenter will call this to let the viewmodel to alert the view
     public void firePropertyChanged(){
         support.firePropertyChange("state", null, this.state);
@@ -26,7 +34,24 @@ public class TopGenreViewModel extends ViewModel {
     public void addPropertyChangeListener(PropertyChangeListener listener){
         support.addPropertyChangeListener(listener);
     }
+
     public TopGenreState getState() {
         return state;
+    }
+
+    @Override
+    public JPanel getViewPanel() {
+        // Create and configure the JPanel for the top genre view
+        JPanel topGenrePanel = new JPanel();
+        topGenrePanel.setLayout(new BorderLayout());
+
+        // Add components representing the top genre view
+        JLabel titleLabel = new JLabel("Top Genres");
+        // Add other UI components as needed
+
+        topGenrePanel.add(titleLabel, BorderLayout.NORTH);
+        // Add other components to the panel
+
+        return topGenrePanel;
     }
 }
