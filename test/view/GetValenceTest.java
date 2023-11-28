8b2c5c274a1cb85697ead6a7564f906795d75d0c
yoonie-jang <yoonie.jang@mail.utoronto.ca>
2023-11-28 00:41:05 -0500

    Added a test for exceptions in the get valence dao method + changed this method to properly handle the exception. Also moved some javadocs to be before the method (instead of inside)

--- test/view/GetValenceTest.java
+++ test/view/GetValenceTest.java
@@ -3,18 +3,30 @@ package view;
 import api.SpotDevelopDB;
 import app.Main;
 import data_access.UserDataAccessObject;
+import org.junit.Test;
 
 import javax.swing.*;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 public class GetValenceTest {
 
-    String id = "312x6t6bf2nas7jiahsll6ajmjba"; // this is the id for my tester account
+    String emptyId = "312x6t6bf2nas7jiahsll6ajmjba"; // this is the id for my empty tester account
     String songId = "11dFghVXANMlKmJXsNCbNl"; // sample song id provided by spotify
     UserDataAccessObject userDataAccessObject = new UserDataAccessObject();
     SpotDevelopDB api = new SpotDevelopDB();
     @org.junit.Test
     public void testApiGetValence() throws Exception {
         assert(api.get_valence(songId).equals("0.625")); // sample song has 0.625 valence
+    }
+
+    @Test
+    public void testGetValenceEmptyAccount() {
+        // test that right exception is thrown
+        Exception exception = assertThrows(Exception.class,
+                () -> userDataAccessObject.getValence(emptyId, "medium-frame"));
+        assertEquals("Listen to some music bro!", exception.getMessage());
     }
 
 }
