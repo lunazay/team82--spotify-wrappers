5a24c348db64f7c091e5f54b1fa44cffda146c8c
ashleychristen <ashley.christendat@mail.utoronto.ca>
2023-11-28 13:18:11 -0500

    working on fixing api call

--- test/use_case/TopArtistsTest.java
+++ test/use_case/TopArtistsTest.java
@@ -16,7 +16,7 @@ public class TopArtistsTest {
     public static void main(String[] args) throws IOException {
         SpotDevelopDB api = new SpotDevelopDB();
         //System.out.println(api.getAuthorizationLink());
-        System.out.println(api.getAuthorizationToken("AQBCsPsKpEtTwzqMWZuMbQ0EgaXNQjPMjNWy5XW8dQQLlZZJxefETc5uTkbYevUhwCHDGUBZpVpZWYttEwFcaz00fz4kXBpLOvtYSfwwN3q6xE8vs5mEe-yi320G1jpY_3fq6TdnJ9_xeoQLeYeVlTrgTgga43A_6AF9vL7V1iQ3x3QpqPyKMd8Ux0mofZOZcIXGuvAmvN3ajk9iRrFb41cSgJdkHjTCN53qH92NhFzbPbvOO5b7kts5gDxRM4GGW0T9BRKrhRvNmyiD8Ah6qoGx029NeqohBiUyDlf4gyDBDXbkewzrFR0T5po49REI-Q4"));
+        System.out.println(api.getAuthorizationToken("AQD3XOiJAoMiLYBejCoph5gfYyjT6qnojrfnMWkDW35WQVtO3_lt7l6L0CMjUqX-ongTM_Z8225uMkjkE32IhB7IisHYJv0ma5nDziUCuw4_AlrLwWZKbahftKOxfN8z7T-56kbYyeLNzeTct95L9VwFuOqJJhL9tHWFbNWw_b5GOyX7KM9MaHsHXUUpvPjee_g6KtQvWmLUZAjU2flTz1_EwK0SwoJxz7zHBVRQcfdQYNWyDPkwPXIgEUViYI33IaIecst3NlHSar53Xb8ea-AVHsCcIQP0lxpK8wl_AQ6CaHN6F08vEsUK16ItmNeHBSk"));
     }
 
     @org.junit.Test
