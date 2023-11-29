7b8934aebed24c807043c0fa71c684c3c3e25dda
jackwiebe0 <jackwiebe@me.com>
2023-11-29 17:38:34 -0500

    added a few more methods in the test file

--- test/use_case/TopArtistsTest.java
+++ test/use_case/TopArtistsTest.java
@@ -16,11 +16,21 @@ public class TopArtistsTest {
         System.out.println(api.getAuthorizationToken("AQCGSB3LXNNDCb40PqWkpkIaenqg1DE5yqpS3wkRJneTLKwWaXipA-zcKSKpDLfhMsdUIaq-V2KOs-0mTBMzAwDUphm-4XEiHNngCZCDH-VuH6k3xwvRiilIcgP0O2RUA8LBzncLy_EMpTwW_OdW9ShfrA6MBNttWkwo36GceEQvgqKs8xDgMxoKcfC3eIaMJJWnnzYI3xnduGGBjeB_GNFdrU1pbIurQe76ZyzM5ew_GJToqylr2o2HAa7A9-jjMVUxjdLLuH6gsH8fg4MuSoh_2lsRiCEJTgOkhIYLlTb0oLW-lpq8qTAQwlXzvep1QAI"));
     }
 
+    public Artist[] getTopArtists() throws Exception {
+        UserDataAccessObject userDataAccessObject = new UserDataAccessObject();
+        Artist[] topArtists = userDataAccessObject.getTopArtists("0wn1qh223kptm533gbxyin4mh", "long_term");
+        return topArtists;
+    }
+
     @org.junit.Test
     public void testTopArtist() throws Exception {
-        UserDataAccessObject userDataAccessObject = new UserDataAccessObject();
-
-        Artist[] topArtists = userDataAccessObject.getTopArtists("0wn1qh223kptm533gbxyin4mh", "long_term");
+        Artist[] topArtists = getTopArtists();
         assert(Objects.equals(topArtists[0].getName(), "Paloma Faith"));
+    }
+
+    @org.junit.Test
+    public void testTopFiftyArtists() throws Exception {
+        Artist[] topArtists = getTopArtists();
+        assert(topArtists.length == 50);
     }
 }
