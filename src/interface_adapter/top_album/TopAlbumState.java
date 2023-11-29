f9b03af131776f3a383ef7e6dd9c766eb19bd5bc
Veda Kesarwani <vedakesarwani@gmail.com>
2023-11-29 15:22:59 -0500

    adding getter method

--- src/interface_adapter/top_album/TopAlbumState.java
+++ src/interface_adapter/top_album/TopAlbumState.java
@@ -22,6 +22,12 @@ public class TopAlbumState {
         this.topAlbumNames = topAlbumNames;
     }
 
+    public List<String> getTopAlbumNames(List<String> topAlbumNames) {
+
+        return topAlbumNames;
+
+    }
+
     public void setError(String error){
 
         this.error = error;
