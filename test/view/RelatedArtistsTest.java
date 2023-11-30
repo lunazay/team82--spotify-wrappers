c0a9fd9160676a0d4b79fc3ec5aeda7aaac63e78
ashleychristen <ashley.christendat@mail.utoronto.ca>
2023-11-29 23:32:16 -0500

    still trying to test

--- test/view/RelatedArtistsTest.java
+++ test/view/RelatedArtistsTest.java
@@ -1,6 +1,7 @@
 package view;
 
 import api.SpotDevelopDB;
+import data_access.UserDataAccessObject;
 import entity.Artist;
 
 import javax.swing.*;
@@ -16,20 +17,25 @@ import java.util.List;
 
 public class RelatedArtistsTest {
 
-    SpotDevelopDB api = new SpotDevelopDB();
     private final String timeframe = "short_term";
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         SpotDevelopDB api = new SpotDevelopDB();
+        UserDataAccessObject user = new UserDataAccessObject();
+
         System.out.println(api.getAuthorizationLink());
-        String authCode = "AQD8tCOuYth3H3LLWyfJOQb0ScNXj-bRBqvzNOFPt3_5gLQERNltntdoY8j2X5CrfUZSHaJVaz2UaU0WJtdR3nmwe986mIuRoPc2B5Mn1gfnqDE9GM8Om88XlJDvc83AwHIIK4yWNoLMWoWZO2Gxbxnk3Kmn2mmopYAUMh3YjQUg4I1ZhEYYLLJ6a4IcZNvNaWQ0TtarI3v5ZZhIIifq5L56G2fVDc1ZzbZxZs9HobbLSAyN4Id2kCNDe4mwkiB5C8IRG9GD2UsjTEoQslsLumTnP4vjIpTdvaHPHVrsXaNunhMxTF4pJJPRm231ZN5QeR8";
-        System.out.println(api.getAuthorizationToken(authCode));
+        //String authCode = "AQDw4FdjcQ6UpU8pnvUhUulPcqzxT8nJLVu3hVWd4EYLfmQ4VOqiMxzuWGDY-8HglMAzpVW9g3LppICJulqbQa5IGhALoWJJGrDItWBKqpFfA4MsTkxm-IWOzDQNiAq66hO9lhuyBPn4KDtsiba-ajIgrKRSVjx24X9OkAtj7Lc-s37N_V6mENyqzep1bqIdE1Vq77mfmAeTxIQ6Lz24Rh8Rge-bRa266Ep8LfEWYO4zpuWJHL0sPxRxHAH-AU_Buouy8A5IDJYzMUprt54veUtzo8Je2vaU1lXpScMVaA1Hg0Z6sWQpqXejDB0THEoJT1s";
+        //System.out.println(api.getAuthorizationToken(authCode));
         System.out.println(api.getUserId());
+
+        System.out.println(user.getTopArtists("31gqehqotfmiji3bbkreneh7d3ia","medium_term"));
+        System.out.println(user.getTopGenres("31gqehqotfmiji3bbkreneh7d3ia", "short_term"));
     }
-    @org.junit.Test
-    public void testAPIGetRelatedArtists() throws Exception{
-        Artist[] artists = api.getTopArtists(timeframe);
-        List<String> relatedArtists = new ArrayList<>();
-        assert(artists[0].getRelatedArtists().equals(relatedArtists));
-    }
+
+//    @org.junit.Test
+//    public void testAPIGetRelatedArtists() throws Exception{
+//        Artist[] artists = api.getTopArtists(timeframe);
+//        List<String> relatedArtists = new ArrayList<>();
+//        assert(artists[0].getRelatedArtists().equals(relatedArtists));
+//    }
 
 }
