b2c28538b4a519173bb5b6f67eee1a6d2b01fb4e
ashleychristen <ashley.christendat@mail.utoronto.ca>
2023-12-01 17:37:23 -0500

    trying to fix top genre bug

--- src/data_access/UserDataAccessObject.java
+++ src/data_access/UserDataAccessObject.java
@@ -21,7 +21,6 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class UserDataAccessObject implements TopSongsDataAccessInterface, TopGenreDataAccessInterface,
         GetValenceDataAccessInterface, RelatedArtistsDataAccessInterface, TopArtistsDataAccessInterface, TopAlbumDataAccessInterface,
