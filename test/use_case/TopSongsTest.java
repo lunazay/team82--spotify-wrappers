81990537d7998db30de42533829482b23db6df28
Veda Kesarwani <vedakesarwani@gmail.com>
2023-12-01 11:35:41 -0500

    editing

--- test/use_case/TopSongsTest.java
+++ test/use_case/TopSongsTest.java
@@ -1,13 +1,4 @@
-package use_case;
 
-import api.SpotDevelopDB;
-import data_access.UserDataAccessObject;
-import entity.Song;
-
-import java.io.IOException;
-import java.util.Objects;
-
-public class TopSongsTest {
     public static void main(String[] args) throws IOException {
         SpotDevelopDB api = new SpotDevelopDB();
         System.out.println(api.getAuthorizationToken("AQBFvYvoO3abMJA5qSHB4zhqigaktLkKP4_duKVqSsINbw4RIC7AFgYEjpjV8ZStswR3paWK5a9jQV-3o8dWxLB-f2AgsoCDeRO1PUGpLl42J7jwhrbXpKO3tPeUVxUHIje4aXOwuRSLGTnWWCfrg0eZ9M5tsi2mKanJgL5N1nzJCaIDUgz_CuwaqU2zr61BwqjBh-xdaoxQDmujACR-2kzIGMyULEsTPO4J6LCPnm0EsD3TgADCh4v0JftDs5ErQxivt44eTXYsSDXXFhghGaUYAAI66u3Hokwgcqg3wJCHvyQIe8ejg8S1q7mUqmM4iaU"));
@@ -20,4 +11,4 @@ public class TopSongsTest {
         Song[] topSongs = userDataAccessObject.getTopSongs("0wn1qh223kptm533gbxyin4mh", "long_term");
         assert(Objects.equals(topSongs[0].getName(), "Living with a Stranger"));
     }
-}
+
