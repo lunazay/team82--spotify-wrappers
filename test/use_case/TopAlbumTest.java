9b10ebe5c8fea76b9fab8a22b656a94dfb1e1329
Veda Kesarwani <vedakesarwani@gmail.com>
2023-11-30 21:24:09 -0500

    album test

--- test/use_case/TopAlbumTest.java
+++ test/use_case/TopAlbumTest.java
@@ -1,11 +1,46 @@
 package use_case;
 
+import api.SpotDevelopDB;
+import data_access.UserDataAccessObject;
+import entity.Album;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
 import static org.junit.Assert.*;
 
 public class TopAlbumTest {
+
+    String id = "..."; // TODO get id
+    public static void main(String[] args) throws IOException {
+
+        SpotDevelopDB api = new SpotDevelopDB();
+        String myAuthCode = "..."; // TODO get my code
+        System.out.println(api.getAuthorizationToken(myAuthCode));
+
+    }
+
+    public ArrayList<Album> getTopAlbums() throws Exception {
+
+        UserDataAccessObject userDataAccessObject = new UserDataAccessObject();
+        ArrayList<Album> topAlbums = userDataAccessObject.getTopAlbums(id, "long_term");
+        return topAlbums;
+
+    }
+
+    @org.junit.Test
+    public void testTopAlbum() throws Exception {
+
+        ArrayList<Album> topAlbums = getTopAlbums();
+        assert(Objects.equals(topAlbums.get(0).getName(), "...")); //TODO input my top album
+
+    }
+
 }
+
+
