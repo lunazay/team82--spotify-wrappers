e2e6c999fb08290e343ef99e4d75612c43c10c49
jackwiebe0 <jackwiebe@me.com>
2023-12-01 21:52:25 -0500

    rearrange helper methods

--- test/view/LoggedInTest.java
+++ test/view/LoggedInTest.java
@@ -10,49 +10,6 @@ import static org.junit.Assert.assertNotNull;
 public class LoggedInTest {
 
     /**
-     * Get the button at the given index.
-     * @param buttonIndex The placement of the button (beginning at index 0).
-     * @return The button in index buttonIndex.
-     */
-    public static JButton getButton(int buttonIndex) {
-        JFrame app = null;
-
-        Window[] windows = Window.getWindows();
-        for (Window window : windows) {
-            if (window instanceof JFrame) {
-                app = (JFrame) window;
-            }
-        }
-
-        // Test fails if the window does not appear.
-        assertNotNull(app);
-        JPanel buttons = getButtonsPanel(app);
-
-        return (JButton) buttons.getComponent(buttonIndex);
-    }
-
-    /**
-     * Helper method to return the buttons panel from the LoggedInView.
-     *
-     * @param app the JFrame of the app.
-     * @return the JPanel with each button as a component.
-     */
-    public static JPanel getButtonsPanel(JFrame app) {
-        // Root of the app
-        Component root = app.getComponent(0);
-        // All the content of the window
-        Component cp = ((JRootPane) root).getContentPane();
-        // Panel Version of the Content Pane
-        JPanel jPanel1 = (JPanel) cp;
-        // The Component of the Panel contains the information on the page
-        JPanel jPanel2 = (JPanel) jPanel1.getComponent(0);
-        // Component 0 is LoginView, Component 1 is LoggedInView
-        LoggedInView loggedInView = (LoggedInView) jPanel2.getComponent(1);
-        // Component 0 is the label of the window, Component 1 is the buttons
-        return (JPanel) loggedInView.getComponent(1);
-    }
-
-    /**
      * Tests that the Short term button appears and is where it should be.
      */
     @org.junit.Test
@@ -91,4 +48,50 @@ public class LoggedInTest {
         JButton button = getButton(3);
         assert(button.getText().equals("Done"));
     }
+
+    // Helper methods:
+
+    /**
+     * Get the button at the given index.
+     * @param buttonIndex The placement of the button (beginning at index 0).
+     * @return The button in index buttonIndex.
+     */
+    private static JButton getButton(int buttonIndex) {
+        JFrame app = null;
+
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+
+        // Test fails if the window does not appear.
+        assertNotNull(app);
+        JPanel buttons = getButtonsPanel(app);
+
+        return (JButton) buttons.getComponent(buttonIndex);
+    }
+
+    /**
+     * Helper method to return the buttons panel from the LoggedInView.
+     *
+     * @param app the JFrame of the app.
+     * @return the JPanel with each button as a component.
+     */
+    private static JPanel getButtonsPanel(JFrame app) {
+        // Root of the app
+        Component root = app.getComponent(0);
+        // All the content of the window
+        Component cp = ((JRootPane) root).getContentPane();
+        // Panel Version of the Content Pane
+        JPanel jPanel1 = (JPanel) cp;
+        // The Component of the Panel contains the information on the page
+        JPanel jPanel2 = (JPanel) jPanel1.getComponent(0);
+        // Component 0 is LoginView, Component 1 is LoggedInView
+        LoggedInView loggedInView = (LoggedInView) jPanel2.getComponent(1);
+        // Component 0 is the label of the window, Component 1 is the buttons
+        return (JPanel) loggedInView.getComponent(1);
+    }
+
 }
--- test/view/LoginTest.java
+++ test/view/LoginTest.java
@@ -10,49 +10,6 @@ import static org.junit.Assert.assertNotNull;
 public class LoginTest {
 
     /**
-     * Verify that the desired button is present.
-     * @param buttonIndex The placement of the button (beginning at index 0).
-     * @return The button in index buttonIndex.
-     */
-    public JButton getButton(int buttonIndex) {
-        JFrame app = null;
-
-        Window[] windows = Window.getWindows();
-        for (Window window : windows) {
-            if (window instanceof JFrame) {
-                app = (JFrame) window;
-            }
-        }
-
-        // Test fails if the window does not appear.
-        assertNotNull(app);
-        JPanel buttons = getButtonsPanel(app);
-
-        return (JButton) buttons.getComponent(buttonIndex);
-    }
-
-    /**
-     * Helper method to return the buttons panel from the LoginView.
-     *
-     * @param app the JFrame of the app.
-     * @return the JPanel with each button as a component.
-     */
-    public static JPanel getButtonsPanel(JFrame app) {
-        // Root of the app
-        Component root = app.getComponent(0);
-        // All the content of the window
-        Component cp = ((JRootPane) root).getContentPane();
-        // Panel Version of the Content Pane
-        JPanel jPanel1 = (JPanel) cp;
-        // The Component of the Panel contains the information on the page
-        JPanel jPanel2 = (JPanel) jPanel1.getComponent(0);
-        // Component 0 is LoginView, Component 1 is LoggedInView
-        LoginView loginView = (LoginView) jPanel2.getComponent(0);
-        // Component 0 is the label of the window, Component 1 is the input field, Component 2 is the buttons
-        return (JPanel) loginView.getComponent(2);
-    }
-
-    /**
      * Tests that the "Start" button appears and is where it should be.
      */
     @org.junit.Test
@@ -81,4 +38,50 @@ public class LoginTest {
         JButton button = getButton(2);
         assert(button.getText().equals("Cancel"));
     }
+
+    // Helper methods:
+
+    /**
+     * Verify that the desired button is present.
+     * @param buttonIndex The placement of the button (beginning at index 0).
+     * @return The button in index buttonIndex.
+     */
+    private JButton getButton(int buttonIndex) {
+        JFrame app = null;
+
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+
+        // Test fails if the window does not appear.
+        assertNotNull(app);
+        JPanel buttons = getButtonsPanel(app);
+
+        return (JButton) buttons.getComponent(buttonIndex);
+    }
+
+    /**
+     * Helper method to return the buttons panel from the LoginView.
+     *
+     * @param app the JFrame of the app.
+     * @return the JPanel with each button as a component.
+     */
+    private static JPanel getButtonsPanel(JFrame app) {
+        // Root of the app
+        Component root = app.getComponent(0);
+        // All the content of the window
+        Component cp = ((JRootPane) root).getContentPane();
+        // Panel Version of the Content Pane
+        JPanel jPanel1 = (JPanel) cp;
+        // The Component of the Panel contains the information on the page
+        JPanel jPanel2 = (JPanel) jPanel1.getComponent(0);
+        // Component 0 is LoginView, Component 1 is LoggedInView
+        LoginView loginView = (LoginView) jPanel2.getComponent(0);
+        // Component 0 is the label of the window, Component 1 is the input field, Component 2 is the buttons
+        return (JPanel) loginView.getComponent(2);
+    }
+
 }
