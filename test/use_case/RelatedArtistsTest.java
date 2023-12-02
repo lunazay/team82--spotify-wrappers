e06aae926c5953074152dcf03da02811f6db0d30
ashleychristen <ashley.christendat@mail.utoronto.ca>
2023-12-01 21:15:15 -0500

    added java doc

--- test/use_case/RelatedArtistsTest.java
+++ test/use_case/RelatedArtistsTest.java
@@ -27,6 +27,12 @@ public class RelatedArtistsTest {
         System.out.println(api.getAuthorizationToken("AQA_Hqap0MiTcGUfM-HEX7QcnBeVzD-G19rw9oX1dwIp7Et3b820SYQrLgC87dzEpjE4q30dSiUwwWgSSYVv5pNopyFSX5EBS2i0o7MM9b-yCMuE6gJnANE9HRy6wM8nv-hBoat1_2fDfln5PcLk9mG23ZBeZuZKeGCQZ2aY9rqRBf4sZAEP3eeE8CM6IQ00-7R7_s3OHVUuX0sHSZ_AZpwPMDnPKDXloXb9m0pmXgPYdRTCcteb7GeoImNYAZ4qy0vaZuUB3kPBxT0SwE4lp937OwWMqUWtYV8WPgzrA5ixYfqM0oOrYRqEsV7T0-xcU7M"));
     }
 
+    /**
+     * Retrieves the top genres for a user.
+     *
+     * @return List of strings of top artist's related artists.
+     * @throws Exception If an error occurs during data retrieval.
+     */
     public List<String> getRelatedArtists() throws Exception{
         UserDataAccessObject userDataAccessObject = new UserDataAccessObject();
         List<String> relatedArtists = userDataAccessObject.getRelatedArtists("luna987654321", "short_term");
